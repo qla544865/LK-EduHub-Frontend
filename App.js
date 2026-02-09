74b6b86b9bf8772ea234ef8ae622768a49c589d9
qla544865 <qla544865@gmail.com>
2026-02-09 17:20:29 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,107 @@
+import { useRef, useState } from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+} from "react-native-safe-area-context";
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <MainApp />
+    </SafeAreaProvider>
+  );
+}
+
+function MainApp() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef(null);
+  const [photo, setPhoto] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+  if (!permission) return <View />;
+
+  if (!permission.granted) {
+    return (
+      <SafeAreaView style={styles.center}>
+        <Text>Need camera permission</Text>
+        <Button title="Grant permission" onPress={requestPermission} />
+      </SafeAreaView>
+    );
+  }
+
+  const takePhoto = async () => {
+    const result = await cameraRef.current.takePictureAsync({
+      quality: 0.8,
+      skipProcessing: true,
+    });
+    console.log("PHOTO:", result.uri);
+    setPhoto(result);
+  };
+
+  const uploadPhoto = async () => {
+    setUploading(true);
+
+    const formData = new FormData();
+    formData.append("file", {
+      uri: photo.uri,
+      name: "photo.jpg",
+      type: "image/jpeg",
+    });
+
+    try {
+      const res = await fetch("http://192.168.1.228:3000/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await res.json();
+      alert("Upload success:\n" + data.url);
+    } catch (err) {
+      console.error(err);
+      alert("Upload failed");
+    }
+
+    setUploading(false);
+  };
+
+  if (photo) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image source={{ uri: photo.uri }} style={styles.preview} />
+
+        <View style={styles.controls}>
+          <Button title="Upload" onPress={uploadPhoto} disabled={uploading} />
+          <View style={{ height: 10 }} />
+          <Button title="Retake" onPress={() => setPhoto(null)} />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <CameraView style={styles.camera} ref={cameraRef} />
+
+      <View style={styles.controls}>
+        <Button title="Take Photo" onPress={takePhoto} />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  camera: { flex: 1 },
+  preview: { flex: 1 },
+  controls: {
+    paddingVertical: 16,
+    alignItems: "center",
+  },
+  center: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ README.md
@@ -0,0 +1,20 @@
+```
+## Cài đặt
+npm install
+
+## Chạy
+npm run dev
+
+## Build
+npm run build
+```
+
+##### Note: branch đặt tên của ai up code lên
+```
+git init
+git add README.md
+git commit -m "commit"
+git branch -M <branch>
+git remote add origin https://github.com/qla544865/LK-EduHub-Frontend.git
+git push -u origin <branch>
+```
